23151a96fc7ff04e9d10dd513ed7f7adeb57729b
pariosa <ariosa@gmail.com>
2017-10-22 12:52:55 -0400

    Update app.php

--- config/app.php
+++ config/app.php
@@ -177,7 +177,7 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
         //cloud convert
-        RobbieP\CloudConvertLaravel\CloudConvertLaravelServiceProvider::class
+        //RobbieP\CloudConvertLaravel\CloudConvertLaravelServiceProvider::class
     ],
 
     /*
